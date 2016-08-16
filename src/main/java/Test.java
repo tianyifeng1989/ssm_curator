2dcab2f08d56e035fa47041f37531156be6e0c2e
zhan <zhan@zhan-PC>
2016-08-16 13:49:35 +0800

    d333 111

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,8 +1,7 @@
 
 public class Test {
 public static void main(String[] args) {
-	System.out.println("8888888888888888888888888888888888888");
-	System.out.println("3333333333333333333");
+	System.out.println("8888888888881111");
 	System.out.println("ppppppppppppppppppppp");
 }
 }
