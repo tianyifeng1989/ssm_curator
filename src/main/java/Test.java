21391540b2028291a5f2e137b114114d9803195e
zhan <zhan@zhan-PC>
2016-08-17 01:21:09 +0800

    22222222

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,8 +1,8 @@
 
 public class Test {
 public static void main(String[] args) {
-	System.out.println("8888888 2222222222222");
-	Object obj = "2222222222222";
-	System.out.println("ppppppp 2222222222");
+	System.out.println("8888888 222222222222");
+	Object obj = "22222222222222222";
+	System.out.println("ppppppp 22222222222222");
 }
 }
