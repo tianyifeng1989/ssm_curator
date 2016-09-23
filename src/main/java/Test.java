d4bf292ce696327291b7960fc19117f2f44f80a2
zhanyuanwen <1470458044@qq.com>
2016-09-23 22:19:31 +0800

    1111

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -6,7 +6,6 @@ public static void main(String[] args) {
 	System.out.println("ppppppp 333333333332222222");
 	System.out.println("8888888 222222222222");
 	Object obj = "22222225555552222";
-	System.out.println("ppppppp 22222222222222");
 
 }
 }
